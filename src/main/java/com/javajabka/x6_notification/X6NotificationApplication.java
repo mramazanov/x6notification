0ffca658b06ebb6ce08c48073186a46cfa23cd33
Admin <mramazanov@outlook.com>
2025-03-20 10:33:45 +0300

    init

---
+++ src/main/java/com/javajabka/x6_notification/X6NotificationApplication.java
@@ -0,0 +1,13 @@
+package com.javajabka.x6_notification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class X6NotificationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(X6NotificationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/javajabka/x6_notification/X6NotificationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javajabka.x6_notification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class X6NotificationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
